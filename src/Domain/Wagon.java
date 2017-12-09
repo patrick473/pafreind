9af9cc54a4ae69d73c77176c24ff5b0ce6916dfa
patrick kottman <patkottman@gmail.com>
2017-12-09 13:35:11 +0100

    uhuh
    uhuh

--- src/Domain/Wagon.java
+++ src/Domain/Wagon.java
@@ -2,5 +2,13 @@ package Domain;
 
 public class Wagon extends Component {
 
+    Integer amountOfSeats;
+    Integer wagonID;
 
+    public Wagon(String name, Integer position, Integer amountOfSeats, Integer wagonID) {
+        super(name, position);
+        if (amountOfSeats== 0){ amountOfSeats=20;}
+        this.amountOfSeats = amountOfSeats;
+        this.wagonID = wagonID;
+    }
 }
