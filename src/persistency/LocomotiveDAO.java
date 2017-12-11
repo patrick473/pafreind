fc0adb1edfda4aeb2b702a21e7d6a1e2ac398586
thomasboy020 <thomasboy020@gmail.com>
2017-12-11 16:36:55 +0100

    LocomotiveDAO v2

--- src/persistency/LocomotiveDAO.java
+++ src/persistency/LocomotiveDAO.java
@@ -2,9 +2,14 @@ package persistency;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import Domain.Locomotive;
 import Domain.Train;
+
 
 
 public class LocomotiveDAO extends BaseDAO {
@@ -15,7 +20,7 @@ public class LocomotiveDAO extends BaseDAO {
 
 			PreparedStatement pstmt = myConn.prepareStatement(
 					"INSERT into locomotive(trainid, name, locomotiveid) VALUES(?,?,?)");
-		//	pstmt.setInt(1, train.getTrainID());
+			pstmt.setInt(1, train.getTrainID());
 			pstmt.setString(2, locomotive.getName());
 			pstmt.setInt(3, locomotive.getLocomotiveID());
 			pstmt.executeQuery();
@@ -26,5 +31,54 @@ public class LocomotiveDAO extends BaseDAO {
 			exc.printStackTrace();
 		}
 
+	}
+	
+	public List<Locomotive> findAlleLocomotives() {
+		List<Locomotive> locomotivelist = new ArrayList<Locomotive>();
+		try {
+			Connection myConn = BaseDAO.getConnection();
+
+			Statement myStmt = myConn.createStatement();
+
+			ResultSet myRs = myStmt.executeQuery("select * from locomotive");
+
+			while (myRs.next()) {
+				Locomotive s = new Locomotive(myRs.getString("name"),
+						myRs.getInt("locomotiveid"));
+				locomotivelist.add(s);
+				
+				
+			}
+			myConn.close();
+
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+		return locomotivelist;
+	}
+	
+	public Locomotive findLocomotive(int trainid) {
+		Locomotive p = null;
+		try {
+			Connection myConn = BaseDAO.getConnection();
+
+			PreparedStatement pstmt = myConn.prepareStatement("SELECT * FROM locomotive WHERE trainid = ?");
+			pstmt.setInt(1, trainid);
+			pstmt.executeQuery();
+
+			ResultSet rs = pstmt.getResultSet();
+
+			while (rs.next()) {
+				int trainid1 = rs.getInt("trainid");
+				String name1 = rs.getString("name");
+				int locomotiveid1 = rs.getInt("locomotiveid");
+				Locomotive locomotive = new Locomotive(trainid1, name1, locomotiveid1);
+
+				p = locomotive;
+			}
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+		return p;
 	}
 }
