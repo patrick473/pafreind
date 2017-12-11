55871e75e327eeb977de0d6a5187b80398e59457
patrick kottman <patkottman@gmail.com>
2017-12-11 16:23:59 +0100

    rrrrr
    rres

--- src/persistency/LocomotiveDAO.java
+++ src/persistency/LocomotiveDAO.java
@@ -4,17 +4,18 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 
 import Domain.Locomotive;
+import Domain.Train;
 
 
 public class LocomotiveDAO extends BaseDAO {
 
-	public void addLocomitive(Locomotive locomotive) {
+	public void addLocomitive(Train train, Locomotive locomotive) {
 		try {
 			Connection myConn = BaseDAO.getConnection();
 
 			PreparedStatement pstmt = myConn.prepareStatement(
 					"INSERT into locomotive(trainid, name, locomotiveid) VALUES(?,?,?)");
-			pstmt.setInt(1, locomotive.getTrainID());
+		//	pstmt.setInt(1, train.getTrainID());
 			pstmt.setString(2, locomotive.getName());
 			pstmt.setInt(3, locomotive.getLocomotiveID());
 			pstmt.executeQuery();
