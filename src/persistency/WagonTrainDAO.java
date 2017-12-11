c1bc8591733327fa96d95d29bd18f4d8f3a2b9c5
thomasboy020 <thomasboy020@gmail.com>
2017-12-11 16:59:44 +0100

    wagontrainDAO

---
+++ src/persistency/WagonTrainDAO.java
@@ -0,0 +1,47 @@
+package persistency;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import Domain.Train;
+import Domain.Wagon;
+
+
+
+public class WagonTrainDAO extends BaseDAO {
+	
+	public void addWagonTrain(Train train, Wagon wagon) {
+		try {
+			Connection myConn = BaseDAO.getConnection();
+
+			PreparedStatement pstmt = myConn.prepareStatement(
+					"INSERT into wagontrain(trainid, wagonid) VALUES(?,?)");
+			pstmt.setInt(1, train.getTrainID());
+			pstmt.setInt(2, wagon.getWagonID());
+			pstmt.executeQuery();
+			myConn.close();
+		}
+
+		catch (Exception exc) {
+			exc.printStackTrace();
+		}
+
+	}
+	
+	public void deleteWagonTrain(Train train) {
+		try {
+			Connection myConn = BaseDAO.getConnection();
+
+			PreparedStatement pstmt = myConn.prepareStatement("DELETE FROM wagontrain WHERE trainid = ?");
+			pstmt.setInt(1, train.getTrainID());
+			pstmt.execute();
+			myConn.close();
+		}
+
+		catch (Exception exc) {
+			exc.printStackTrace();
+		}
+
+	}
+
+}
