801f6321a3f7450b2cddfcb821666d166ac9c2a0
patrick kottman <patkottman@gmail.com>
2017-12-11 15:10:33 +0100

    added connection factory

--- src/persistency/BaseDAO.java
+++ src/persistency/BaseDAO.java
@@ -1,4 +1,34 @@
 package persistency;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+
 public class BaseDAO {
+    private DataSource connectionPool;
+
+    public BaseDAO() {
+
+    }
+    public Connection getConnection(){
+        final String url = "jdbc:postgresql://localhost/postgres";
+        final String user = "postgres";
+        final String password = "password";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
 }
--- src/persistency/Test.java
+++ src/persistency/Test.java
@@ -1,7 +1,19 @@
 package persistency;
 
+
+
+
 /**
  * Created by patrick on 12/7/2017.
  */
 public class Test {
-}
+
+    public static void main(String[] args) {
+
+        BaseDAO bdao = new BaseDAO();
+        bdao.getConnection();
+
+
+        }
+    }
+
