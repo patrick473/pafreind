9f4cf7d103e9b137485146537c062a8740d05d0c
thomasboy020 <thomasboy020@gmail.com>
2017-12-14 16:27:28 +0100

    change

--- src/Interface/ConsoleInterface.java
+++ src/Interface/ConsoleInterface.java
@@ -3,6 +3,7 @@ package Interface;
 import java.util.*;
 
 import Domain.Wagon;
+import persistency.TrainDAO;
 import persistency.WagonTrainDAO;
 import persistency.WagonTypeDAO;
 
@@ -15,6 +16,8 @@ public class ConsoleInterface {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
         WagonTypeDAO wdao = new WagonTypeDAO();
+        WagonTrainDAO wtdao = new WagonTrainDAO();
+        TrainDAO tdao = new TrainDAO();
         while ( !"stop".equals( s)) {
 
             String[] splitcheck = s.split(" ");
@@ -23,28 +26,28 @@ public class ConsoleInterface {
                     System.out.println(s.substring(10));
 
                 } else if (s.startsWith("new wagon ") && "numseats".equals(splitcheck[3])) {
-                    System.out.println("avef");
-                    System.out.println(splitcheck[4]);
-                    // new wagon type with specified amount of seats
                     int seats = Integer.parseInt(splitcheck[4]);
                     Wagon wagon = new Wagon(splitcheck[2], seats);
                     wdao.addWagonType(wagon);
                 } else if (s.startsWith("new wagon ")) {
-                    System.out.println(s.substring(10));
-                    // new wagon type with standard amount of seats using controller
+                    Wagon wagon = new Wagon(splitcheck[2], 20);
+                    wdao.addWagonType(wagon);
                 } else if (s.startsWith("add ")&& "to".equals(splitcheck[2])) {
                     System.out.println(s.substring(4));
                     System.out.println(splitcheck[3]);
                     // adding existing wagon to existing train
+                    
                 } else if (s.startsWith("getnumseats train ")) {
                     System.out.println(s.substring(18));
                     // get number of seats of existing train
+                    tdao.getTrainSeats(1);
                 } else if (s.startsWith("getnumseats wagon ")) {
                     System.out.println(s.substring(18));
                     //get number of seats of existing wagon
                 } else if (s.startsWith("delete train ")) {
                     System.out.println(s.substring(13));
                     //delete train, if it doesn't exist dont delete
+                    
                 } else if ("remove".equals(splitcheck[0]) && "from".equals(splitcheck[2])) {
                     System.out.println(splitcheck[1]);
                     System.out.println(splitcheck[3]);
